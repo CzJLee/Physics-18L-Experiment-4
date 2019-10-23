97efb48753f8d52dbdbc611f9ceb54a66cf8eee7
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2019-10-23 13:30:28 -0700

    Added header comments and libraries

---
+++ Projectile Motion in 2D/projectile-motion.py
@@ -0,0 +1,18 @@
+"""
+Physics 18L - Experiment 4 - Day 2
+Christian Lee
+  
+Professor: N. Whitehorn
+TA: Teresa Le
+Lab Date: Thursday, Oct 24, 2019
+UCLA Physics Department
+
+Required libraries: matplotlib, numpy
+
+Projectile motion in 2D
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
