be98bf2ac50d165570a854b0b482fe591ea58b9a
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2019-10-23 12:39:03 -0700

    First commit prng.py. Basic plot working.

---
+++ Psychophysics and Random Numbers/prng.py
@@ -0,0 +1,59 @@
+import random
+from matplotlib import pyplot
+from math import sqrt
+
+seed = random.randrange(1, 2**32)
+
+def lcg():
+	#Linear Congruential Generator
+	#V_(j+1) = ( V_j * A + B ) mod M
+	#A is the multiplier
+	a = 1664525
+	#B is the increment
+	b = 1013904223
+	#M is the modulus
+	m = 2**32
+	#V_j is the seed
+	#Function returns a float in [0, 1)
+
+	global seed #Allow lcg to modify seed
+
+	seed = ((seed * a) + b) % m 
+	return seed / 2**32
+
+# rand = []
+# for i in range(0,10000):
+# 	rand.append(lcg())
+
+# pylab.hist(rand, bins = 100)
+
+# pylab.show()
+
+def ring(rad_a, rad_b, n=10):
+	#Generate a random point that falls within a ring of inner radius rad_a and outer radius rad_b
+	#Assume rad_a < rad_b
+
+	if rad_a == rad_b:
+		raise ValueError("The ring has width zero.")
+
+	if rad_a > rad_b:
+		rad_a, rad_b = rad_b, rad_a
+
+	x = []
+	y = []
+
+	while(len(x) < n):
+		rand_x = random.uniform(0, rad_b) * random.randrange(-1, 2, 2)
+		rand_y = random.uniform(0, rad_b) * random.randrange(-1, 2, 2)
+		if rad_a <= sqrt(rand_x**2 + rand_y**2) <= rad_b:
+			x.append(rand_x)
+			y.append(rand_y)
+
+	pyplot.plot(x, y, "ro")
+	pyplot.draw()
+	pyplot.pause(0.0001)
+	pyplot.clf()
+	
+
+for i in range(0,100):
+	ring(10, 12, 10)
