c6f0b8028536bb0ae3648a9d60023e0193b1e3f5
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2019-10-24 12:17:44 -0700

    Draw frame working with particles appearing.

--- Projectile Motion in 2D/projectile-motion.py
+++ Projectile Motion in 2D/projectile-motion.py
@@ -16,3 +16,75 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+class ParticleBox: 
+	def __init__(self, init_state = [[1, 0, 0, -1]], box_bounds = [-2, 2, -2, 2], ball_size = 16, mass=0.05, gravity=9.81):
+		#init_state is an [N x 4] array, where N is the number of particles:
+		#[[x1, y1, vx1, vy1],
+		#[x2, y2, vx2, vy2], ...]
+
+		#bounds is the size of the box: [xmin, xmax, ymin, ymax]
+
+		#size is the size of the ball
+
+		self.init_state = np.asarray(init_state, dtype=float) #If init_state is not an array of floats, convert into an array containing only floats. 
+		self.m = mass * np.ones(self.init_state.shape[0]) #self.init_state.shape[0] returns the number of particles contained within init_state. np.ones proudces an array full of n 1's where n is the passed variable. The mass of all particles will be set to m. 
+		self.size = ball_size #Set the size of the particle
+		self.state = self.init_state.copy() #Shallow copy the initial state into state, which will store the updates states as the particle moves. 
+		self.bounds = box_bounds
+		self.g = gravity #Gravity
+
+	def step(self, dt):
+		# update positions
+		self.state[:, :2] += dt * self.state[:, 2:]
+
+#Set up initial state
+np.random.seed(0) #Set constant seed.
+init_state = -0.5 + np.random.random((1, 4)) #Randomly Generate (# of particles, # of elements (4 required))
+#All particle dimension start with a position and veloicty between (-0.5, 0.5) produced by a gaussain distribution about 0. 
+#init_state[:, :2] *= 3.9 #Multiply the starting position of each particle by 3.9. 
+
+#Define the working box variable of ParticleBox class. 
+box = ParticleBox()
+
+#Define step size dt. 
+fps = 30
+dt = 1 / fps
+
+#fig determines where the plot is drawn on the screen. 
+fig = plt.figure()
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+	#Adjust screen position
+ax = fig.add_subplot(aspect='equal', autoscale_on=False, xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
+	#Draw axes positions
+
+#Createa variable particles that contain the position of the particles.
+particles, = ax.plot([], [], 'bo', ms=box.size)
+
+# rect is the box edge
+rect = plt.Rectangle(box.bounds[::2], box.bounds[1] - box.bounds[0], box.bounds[3] - box.bounds[2], edgecolor="black", linewidth=2, facecolor="none")
+#Rectangle(bottom left corner coordinates, width, height, ...)
+ax.add_patch(rect)
+
+#Initialize the first frame of the animation
+def init():
+	global box, rect
+	particles.set_data([], [])
+	#rect.set_edgecolor('none')
+	return particles, rect
+
+def animate(i):
+	#Sequential animation step that the animation function uses. The iterable i is not used. 
+	"""perform animation step"""
+	global box, rect, dt, ax, fig
+	box.step(dt) #Take one step forward in time (dt). This updates all particles in box.
+	
+	# update pieces of the animation
+	#rect.set_edgecolor('k')
+	particles.set_data(box.state[:, 0], box.state[:, 1])
+	#particles.set_markersize(box.size)
+	return particles, rect
+
+animate_plot = animation.FuncAnimation(fig, animate, frames=600, interval=10, blit=True, init_func=init)
+#particles and rect are the only objects that actually get plotted. 
+
+plt.show()
